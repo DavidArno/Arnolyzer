845d1b11d79d38a4d7a4d3fa717edce543928691
David Arno <david@davidarno.org>
2015-10-03 14:48:58 +0100

    Initial code commit. Just the template solution at this point and doesn't do anything useful.

---
+++ Arnolyzer.Test/UnitTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Arnolyzer.Test.Helpers;
+using CodeFixVerifier = Arnolyzer.Test.Verifiers.CodeFixVerifier;
+
+namespace Arnolyzer.Test
+{
+    [TestClass]
+    public class UnitTest : CodeFixVerifier
+    {
+
+        //No diagnostics expected to show up
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = @"";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "Arnolyzer",
+                Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 11, 15)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new ArnolyzerCodeFixProvider();
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new ArnolyzerAnalyzer();
+    }
+}
